97519a15a1368422feebcb6a1365333e7fc13819
澤なつみ <n2natsu1218@gmail.com>
2018-05-04 11:45:33 +0900

    :dizzy: interaction between my blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,13 @@
+# coding:utf-8
+
 import hashlib
 import json
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+
+import requests
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -13,80 +19,80 @@ class Blockchain(object):
         # ジェネシスブロックを作る
         self.new_block(previous_hash=1, proof=100)
 
-        def new_block(self, proof, previous_hash=None):
-            """ブロックチェーンに新しいブロックを作る
-            :param proof: <int> プルーフ・オブ・ワークアルゴリズムから得られるプルーフ
-            :param previous_hash:  (オプション) <str> 前のブロックのハッシュ
-            :return: <dict> 新しいブロック"""
+    def new_block(self, proof, previous_hash=None):
+        """ブロックチェーンに新しいブロックを作る
+        :param proof: <int> プルーフ・オブ・ワークアルゴリズムから得られるプルーフ
+        :param previous_hash:  (オプション) <str> 前のブロックのハッシュ
+        :return: <dict> 新しいブロック"""
 
-            block = {
-                'index': len(self.chain) + 1,
-                'timestamp': time(),
-                'transactions': self.current_transactions,
-                'proof': proof,
-                'previous_hash': previous_hash or self.hash(self.chain[-1]),
-            }
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
 
-            # 現在のトランザクションリストをリセット
-            self.current_transactions = []
+        # 現在のトランザクションリストをリセット
+        self.current_transactions = []
 
-            self.chain.append(block)
-            return block
+        self.chain.append(block)
+        return block
 
-        # 新しいトランザクションをリストに加える
-        def new_transaction(self):
-            pass
+    # 新しいトランザクションをリストに加える
+    def new_transaction(self):
+        pass
 
-        # ブロックをハッシュ化する
-        @staticmethod
-        def hash(block):
-            """
-            ブロックのSHA-256ハッシュを作る
-            :param block: <dict> ブロック
-            :return: <str<
-            """
+    # ブロックをハッシュ化する
+    @staticmethod
+    def hash(block):
+        """
+        ブロックのSHA-256ハッシュを作る
+        :param block: <dict> ブロック
+        :return: <str<
+        """
 
-            # 必ず辞書型オブジェクトがソートされている必要がある、そうでないと一貫性のないハッシュとなってしまう
-            block_string = json.dumps(block, sort_keys=True).encode()
-            return hashlib.sha256(block_string).hexdigest()
+        # 必ず辞書型オブジェクトがソートされている必要がある、そうでないと一貫性のないハッシュとなってしまう
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
-        # チェーンの最後のブロックをリターンする
-        @property
-        def last_block(self):
-            return self.chain[-1]
+    # チェーンの最後のブロックをリターンする
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
-        #  新しいトランザクションをリストに加え、そのトランザクションが加えられるブロックのインデックスを返す
-        def new_transaction(self, sender, recipient, amount):
-            """
-            次に採用されるブロックに加える新しいトランザクションを作る
-            :param sender: <str> 送信者のアドレス
-            :param recipient: <str> 受信者のアドレス
-            :param amount: <int> 量
-            :return: <int> このトランザクションを含むブロックのアドレス
-            """
+    #  新しいトランザクションをリストに加え、そのトランザクションが加えられるブロックのインデックスを返す
+    def new_transaction(self, sender, recipient, amount):
+        """
+        次に採用されるブロックに加える新しいトランザクションを作る
+        :param sender: <str> 送信者のアドレス
+        :param recipient: <str> 受信者のアドレス
+        :param amount: <int> 量
+        :return: <int> このトランザクションを含むブロックのアドレス
+        """
 
-            self.current_transactions.append({
-                'sender': sender,
-                'recipient': recipient,
-                'amount': amount,
-            })
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
 
-            return self.last_block['index'] + 1
+        return self.last_block['index'] + 1
 
-        def proof_of_work(self, last_proof):
-            """
-            シンプルなプルーフ・オブ・ワークのアルゴリズム:
-            - hash(pp')の最初の4つが0となるようなp'を探す
-            - p は一つ前のブロックのプルーフ、p'は新しいブロックのプルーフ
-            :param last_proof: <int>
-            :return: <int>
-            """
+    def proof_of_work(self, last_proof):
+        """
+        シンプルなプルーフ・オブ・ワークのアルゴリズム:
+        - hash(pp')の最初の4つが0となるようなp'を探す
+        - p は一つ前のブロックのプルーフ、p'は新しいブロックのプルーフ
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
 
-            proof = 0
-            while self.valid_proof(last_proof, proof) is False:
-                proof += 1
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
 
-            return proof
+        return proof
 
     @staticmethod
     def valid_proof(last_proof, proof):
@@ -96,7 +102,74 @@ class Blockchain(object):
         :return: <bool> 正しければ true, そうでなければ false
         """
 
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigests()
+        guess = '{last_proof}{proof}'.format(last_proof = last_proof, proof = proof).encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
 
         return guess_hash[:4] == "0000"
+
+# ノードを作る
+app = Flask(__name__)
+
+# このノードにグローバルにユニークなアドレスを作る
+node_identifire = str(uuid4()).replace('-', '')
+
+# ブロックチェーンクラスをインスタンス化する
+blockchain = Blockchain()
+
+# メソッドはPOSTで/transactions/newエンドポイントを作る、POSTなのでデータを送信する
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    value = request.get_json()
+
+    # POSTされたデータに必要なデータがあるかを確認
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in value for k in required):
+        return "Missing values", 400
+
+    # 新しいトランザクションを作る
+    index = blockchain.new_transactions(value['sender'], value['recipient'], value['amount'])
+
+    return jsonify(response), 201
+
+# メソッドはGETで/mineエンドポイントを作る
+@app.route('/mine', methods=['GET'])
+def mine():
+    # 次のプルーフを見つけるためのプルーフ・オブ・ワークアルゴリズムを使用する
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # プルーフを見つけたことに対する報酬を得る
+    # 送信者は、採掘者が新しいコインを採掘したことを表すために"0"とする
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifire,
+        amount=1,
+    )
+
+    # チェーンに新しいブロックを加えることで、新しいブロックを採掘する
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': '新しいブロックを採掘しました',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+
+# メソッドはGETで、フルのブロックチェーンをリターンする/chainエンドポイントを作る
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+# port5000でサーバーを起動する
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
